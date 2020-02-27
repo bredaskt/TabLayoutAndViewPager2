4685113adfbe8b05577ef345e72b1257d3b82083
= <=>
2020-02-27 15:42:54 -0300

    Adiciona sharedPreferences para mostrar os slides do tablayout apenas na primeira vez que o usuario abrir o app.

--- app/src/main/java/com/bbreda/tablayoutandviewpager2/MainActivity.kt
+++ app/src/main/java/com/bbreda/tablayoutandviewpager2/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.bbreda.tablayoutandviewpager2
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,14 +12,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkIfUserAlreadyVisitTheTutorial()
+    }
 
+    private fun checkIfUserAlreadyVisitTheTutorial() {
+        var PRIVATE_MODE = 0
+        val PREF_NAME = "if_user_already_visit_tablayout_and_view_pager2"
+        val sharedPref: SharedPreferences = getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+
+        if (sharedPref.getBoolean(PREF_NAME, false)) {
+            val homeIntent = Intent(this, HomeActivity::class.java)
+            startActivity(homeIntent)
+            finish()
+        } else {
+            setupViewPager()
+            val editor = sharedPref.edit()
+            editor.putBoolean(PREF_NAME, true)
+            editor.apply()
+        }
+    }
+
+    private fun createCardAdapter(): ViewPagerAdapter {
+        return ViewPagerAdapter(this)
+    }
+
+    private fun setupViewPager() {
+        setContentView(R.layout.activity_main)
         view_pager.adapter = createCardAdapter()
         TabLayoutMediator(tabs, view_pager,
             TabLayoutMediator.TabConfigurationStrategy { tab, position ->
                 tab.text = "Tab " + (position + 1)
             }).attach()
     }
-    private fun createCardAdapter(): ViewPagerAdapter {
-        return ViewPagerAdapter(this)
-    }
+
 }
